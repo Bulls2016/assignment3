0d36959853d02020fb38110b83c0602b40036394
natwal02 <navinder94@gmail.com>
2016-09-22 14:50:46 +1000

    test file for bookhelper

---
+++ ITC515_NewLibrary/src/library/daos/BookHelperTest.java
@@ -0,0 +1,23 @@
+
+package library.daos;
+
+import junit.framework.TestCase;
+
+/**
+ * @author 11542757
+ *
+ */
+public class BookHelperTest extends TestCase {
+
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	// test for testmakebook 
+        //
+	public void testMakeBook() {
+		fail("Not yet implemented");
+	}
+
+}
