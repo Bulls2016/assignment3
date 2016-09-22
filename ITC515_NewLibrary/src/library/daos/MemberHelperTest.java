c17ec7fb638eb9b9241fffd2331ea99c59860615
natwal02 <navinder94@gmail.com>
2016-09-22 14:53:04 +1000

    test file for memberhelp file

---
+++ ITC515_NewLibrary/src/library/daos/MemberHelperTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package library.daos;
+
+import junit.framework.TestCase;
+
+/**
+ * @author 11542757
+ *
+ */
+public class MemberHelperTest extends TestCase {
+
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+       
+        //
+        // test for testMakeMember
+        //
+	
+	public void testMakeMember() {
+		fail("Not yet implemented");
+	}
+
+}
