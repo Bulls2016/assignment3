9414b395989e490c06d59bcfa51518fcc8355b04
natwal02 <navinder94@gmail.com>
2016-09-22 14:53:58 +1000

    test file for member map daos

---
+++ ITC515_NewLibrary/src/library/daos/MemberMapDAOTest.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package library.daos;
+
+import junit.framework.TestCase;
+
+/**
+ * @author 11580359
+ *
+ */
+public class MemberMapDAOTest extends TestCase {
+
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	//
+        // test for AddMember
+        //
+	public void testAddMember() {
+		fail("Not yet implemented");
+	}
+
+	//
+        // test for GetMemberByID
+        //
+	public void testGetMemberByID() {
+		fail("Not yet implemented");
+	}
+
+	//
+        // test for ListMembers
+        //
+	public void testListMembers() {
+		fail("Not yet implemented");
+	}
+
+	//
+        // test for FindMembersByLastName
+        //
+	public void testFindMembersByLastName() {
+		fail("Not yet implemented");
+	}
+
+	//
+        // test for FindMembersByEmailAddress
+        //
+	public void testFindMembersByEmailAddress() {
+		fail("Not yet implemented");
+	}
+
+	//
+        // test for FindMembersByNames
+        //
+	public void testFindMembersByNames() {
+		fail("Not yet implemented");
+	}
+
+}
